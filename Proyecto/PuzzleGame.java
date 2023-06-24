ba72fe55d9e853b9443c02a8bfdd9bbc0fdb538e
nahinmendoza <112567059+nahinmendoza@users.noreply.github.com>
2023-06-24 00:18:51 -0500

    Create PuzzleGame.java

---
+++ Proyecto/PuzzleGame.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class PuzzleGame {
+    
+    JFrame ventana;
+    public static void main(String[] args) 
+    {
+        new PuzzleGame();
+    }
+
+    PuzzleGame() 
+    {
+        // VENTANA DEL JUEGO
+        ventana = new JFrame("ATRAPA EL CARDUMEN");
+        ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        ventana.setLayout(null);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+    }
+}
