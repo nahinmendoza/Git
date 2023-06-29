bc8e80ba96cb1ae6226664ae0ac4378bb1cf4b3a
nahinmendoza <112567059+nahinmendoza@users.noreply.github.com>
2023-06-28 19:06:01 -0500

    nuevo

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+
+
+public class Cliente {
+
+    String cedula, nombre, apellido;
+    
+    public void setCedula(String c)
+    {
+        cedula = c;
+    }
+
+    public String getCedula()
+    {
+        return cedula;
+    }
+}
